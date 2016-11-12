189807b3d5ea420045142fe0d584655b20346a34
Yutaro Iiyama <yiiyama@mit.edu>
2016-11-12 11:11:48 -0500

    genkintree

--- DataFormats/interface/Objects_genkintree.h
+++ DataFormats/interface/Objects_genkintree.h
@@ -6,16 +6,22 @@
 #include "TVector2.h"
 #include "TString.h"
 #include "Rtypes.h"
+#include <memory>
+#include <set>
+#include <utility>
 class TTree;
 
 namespace genkintree {
 
+  typedef std::pair<unsigned, unsigned> SinglesPos;
   typedef ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<double>> LorentzVectorM;
 
   class Particle {
   public:
     struct array_data {
       static UInt_t const NMAX{256};
+
+      array_data();
 
       Float_t pt[NMAX]{};
       Float_t eta[NMAX]{};
@@ -26,12 +32,26 @@ namespace genkintree {
       void book(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
     };
 
+    Particle();
     Particle(array_data&, UInt_t idx);
     Particle(Particle const&);
-    virtual ~Particle() {}
+    virtual ~Particle();
     Particle& operator=(Particle const&);
+    virtual void setStatus(TTree&, TString const&, Bool_t, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
+    virtual void setAddress(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
+    virtual void book(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
+    virtual void init();
 
     virtual LorentzVectorM p4() const { return LorentzVectorM(pt, eta, phi, 0.); }
+
+  private:
+    static std::vector<std::auto_ptr<array_data>> singlesData_;
+    static SinglesPos singlesPos_;
+    static std::set<SinglesPos> usedSinglesPos_;
+    static SinglesPos const& nextSinglesPos_();
+
+  protected:
+    SinglesPos pos_{-1, -1};
 
   public:
     Float_t& pt;
@@ -39,9 +59,11 @@ namespace genkintree {
     Float_t& phi;
   };
 
-  class Jet : public Particle {
+  class ParticleM : public Particle {
   public:
     struct array_data : public Particle::array_data {
+      array_data();
+
       Float_t mass[NMAX]{};
 
       void setStatus(TTree&, TString const&, Bool_t, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
@@ -49,20 +71,97 @@ namespace genkintree {
       void book(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
     };
 
-    Jet(array_data&, UInt_t idx);
-    Jet(Jet const&);
-    virtual ~Jet() {}
-    Jet& operator=(Jet const&);
+    ParticleM();
+    ParticleM(array_data&, UInt_t idx);
+    ParticleM(ParticleM const&);
+    virtual ~ParticleM();
+    ParticleM& operator=(ParticleM const&);
+    void setStatus(TTree&, TString const&, Bool_t, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE) override;
+    void setAddress(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE) override;
+    void book(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE) override;
+    void init() override;
 
-    LorentzVectorM p4() const { return LorentzVectorM(pt, eta, phi, mass); }
+    LorentzVectorM p4() const override { return LorentzVectorM(pt, eta, phi, mass); }
+
+  private:
+    static std::vector<std::auto_ptr<array_data>> singlesData_;
+    static SinglesPos singlesPos_;
+    static std::set<SinglesPos> usedSinglesPos_;
+    static SinglesPos const& nextSinglesPos_();
 
   public:
     Float_t& mass;
   };
 
+  class Parton : public Particle {
+  public:
+    struct array_data : public Particle::array_data {
+      array_data();
+
+      Char_t pid[NMAX]{};
+
+      void setStatus(TTree&, TString const&, Bool_t, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
+      void setAddress(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
+      void book(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
+    };
+
+    Parton();
+    Parton(array_data&, UInt_t idx);
+    Parton(Parton const&);
+    virtual ~Parton();
+    Parton& operator=(Parton const&);
+    void setStatus(TTree&, TString const&, Bool_t, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE) override;
+    void setAddress(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE) override;
+    void book(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE) override;
+    void init() override;
+
+  private:
+    static std::vector<std::auto_ptr<array_data>> singlesData_;
+    static SinglesPos singlesPos_;
+    static std::set<SinglesPos> usedSinglesPos_;
+    static SinglesPos const& nextSinglesPos_();
+
+  public:
+    Char_t& pid;
+  };
+
+  class Boson : public ParticleM {
+  public:
+    struct array_data : public ParticleM::array_data {
+      array_data();
+
+      UShort_t flavor[NMAX]{};
+
+      void setStatus(TTree&, TString const&, Bool_t, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
+      void setAddress(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
+      void book(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
+    };
+
+    Boson();
+    Boson(array_data&, UInt_t idx);
+    Boson(Boson const&);
+    virtual ~Boson();
+    Boson& operator=(Boson const&);
+    void setStatus(TTree&, TString const&, Bool_t, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE) override;
+    void setAddress(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE) override;
+    void book(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE) override;
+    void init() override;
+
+  private:
+    static std::vector<std::auto_ptr<array_data>> singlesData_;
+    static SinglesPos singlesPos_;
+    static std::set<SinglesPos> usedSinglesPos_;
+    static SinglesPos const& nextSinglesPos_();
+
+  public:
+    UShort_t& flavor;
+  };
+
   class Photon : public Particle {
   public:
     struct array_data : public Particle::array_data {
+      array_data();
+
       Float_t isot[NMAX]{};
 
       void setStatus(TTree&, TString const&, Bool_t, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
@@ -70,26 +169,75 @@ namespace genkintree {
       void book(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
     };
 
+    Photon();
     Photon(array_data&, UInt_t idx);
     Photon(Photon const&);
-    virtual ~Photon() {}
+    virtual ~Photon();
     Photon& operator=(Photon const&);
+    void setStatus(TTree&, TString const&, Bool_t, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE) override;
+    void setAddress(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE) override;
+    void book(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE) override;
+    void init() override;
+
+  private:
+    static std::vector<std::auto_ptr<array_data>> singlesData_;
+    static SinglesPos singlesPos_;
+    static std::set<SinglesPos> usedSinglesPos_;
+    static SinglesPos const& nextSinglesPos_();
 
   public:
     Float_t& isot;
   };
 
+  class Lepton : public Particle {
+  public:
+    struct array_data : public Particle::array_data {
+      array_data();
+
+      Char_t charge[NMAX]{};
+      Float_t minDRJ[NMAX]{};
+
+      void setStatus(TTree&, TString const&, Bool_t, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
+      void setAddress(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
+      void book(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
+    };
+
+    Lepton();
+    Lepton(array_data&, UInt_t idx);
+    Lepton(Lepton const&);
+    virtual ~Lepton();
+    Lepton& operator=(Lepton const&);
+    void setStatus(TTree&, TString const&, Bool_t, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE) override;
+    void setAddress(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE) override;
+    void book(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE) override;
+    void init() override;
+
+  private:
+    static std::vector<std::auto_ptr<array_data>> singlesData_;
+    static SinglesPos singlesPos_;
+    static std::set<SinglesPos> usedSinglesPos_;
+    static SinglesPos const& nextSinglesPos_();
+
+  public:
+    Char_t& charge;
+    Float_t& minDRJ;
+  };
+
   class Met {
   public:
-    Met(TString const& name) : name_(name) {}
+    Met(TString const& name);
     Met(Met const&);
-    virtual ~Met() {}
+    virtual ~Met();
     Met& operator=(Met const&);
 
     void setName(TString const& name) { name_ = name; }
     virtual void setStatus(TTree&, Bool_t, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
+    virtual void setStatus(TTree&, TString const&, Bool_t, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
     virtual void setAddress(TTree&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
+    virtual void setAddress(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
     virtual void book(TTree&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
+    virtual void book(TTree&, TString const&, flatutils::BranchList const& = {"*"}, Bool_t whitelist = kTRUE);
+    virtual void init();
 
     TVector2 v() const { TVector2 vec; vec.SetMagPhi(pt, phi); return vec; }
 
